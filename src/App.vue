ea0a1627d9cdb53952827593e6505c6b65800888
FranBotha1990 <f.botha@outlook.com>
2024-08-25 20:01:41 +0300

    Add storeToRefs to taskStore to simplify.

--- src/App.vue
+++ src/App.vue
@@ -19,21 +19,23 @@
         </nav>
 
         <!-- loading -->
-        <div class="loading" v-if="taskStore.loading">Loading tasks...</div>
+        <div class="loading" v-if="loading">Loading tasks...</div>
 
         <!-- task list -->
         <div class="task-list" v-if="filter === 'all'">
-            <p>You have {{ taskStore.totalCount }} tasks left to do</p>
-            <div v-for="task in taskStore.tasks">
+            <p>You have {{ totalCount }} tasks left to do</p>
+            <div v-for="task in tasks">
                 <TaskDetails :task="task"/>
             </div>
         </div>
         <div class="task-list" v-if="filter === 'favs'">
-            <p>You have {{ taskStore.favCount }} favs left to do</p>
-            <div v-for="task in taskStore.favs">
+            <p>You have {{ favCount }} favs left to do</p>
+            <div v-for="task in favs">
                 <TaskDetails :task="task"/>
             </div>
         </div>
+
+        <button @click="$reset">Reset</button>
 
     </main>
 </template>
@@ -43,6 +45,7 @@
     import TaskDetails from './components/TaskDetails.vue';
     import { useTaskStore } from './stores/TaskStore';
     import TaskForm from './components/TaskForm.vue';
+    import { storeToRefs } from 'pinia';
 
     export default {
         components: { TaskDetails, TaskForm },
@@ -50,12 +53,14 @@
             // Declare a variable, using the useTaskStore, and invoke it ()
             const taskStore = useTaskStore()
 
+            const { tasks, loading, favs, totalCount, favCount } = storeToRefs(taskStore)
+
             // fetch tasks
             taskStore.getTasks()
 
             const filter = ref('all')
 
-            return { taskStore, filter }
+            return { taskStore, filter, tasks, loading, favs, totalCount, favCount }
         }
         
     }
