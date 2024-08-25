797cd1bbfb6b06be08fca29ce18a3f6de4a813ef
FranBotha1990 <f.botha@outlook.com>
2024-08-25 16:52:36 +0300

    Create TaskSctore.js and define its state.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import './assets/main.css'
-
+// Import create pinia function
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Invoke pinia in App
+createApp(App).use(createPinia()).mount('#app')
---
+++ src/stores/TaskStore.js
@@ -0,0 +1,12 @@
+import { defineStore } from "pinia";
+
+// Invoke function to create a store from a variable
+export const useTaskStore = defineStore('taskStore', {
+    state: () => ({
+        tasks: [
+            {id: 1, title: "Buy some milk", isFav: false},
+            {id: 2, title: "Play GloomHaven", isFav: true}
+        ],
+        name: 'Yoshi'
+    })
+})
